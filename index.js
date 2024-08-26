c6287244750902787cb01b75eb7d7550297d23a4
ibrahemassa <oioi.ll58@outlook.com>
2024-08-26 16:50:25 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = 3000;
+const API_URL = "https://api.jikan.moe/v4/anime/";
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/search", async (req, res) => {
+  const query = req.query.q;
+  if (!query) {
+    return res.render("search_results.ejs", {
+      results: [],
+      query: "",
+      error: "No search query provided.",
+    });
+  }
+  try {
+    let searchConfig = {
+      params: {
+        q: query,
+        limit: 15,
+        order_by: "score",
+        sort: "desc",
+      },
+    };
+    const result = await axios.get(API_URL, searchConfig);
+    res.render("search_results.ejs", {
+      results: result.data.data,
+      query,
+      error: null,
+    });
+  } catch (err) {
+    res.render("search_results.ejs", {
+      results: [],
+      query,
+      error: "No results found.",
+    });
+  }
+});
+
+app.get("/anime", async (req, res) => {
+  let id = req.query.id;
+  try {
+    const result = await axios.get(API_URL + id);
+    res.render("anime.ejs", { anime: result.data.data });
+  } catch (err) {
+    res.render("anime.ejs", { anime: err.response.data });
+  }
+});
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Running on localhost:${PORT}`);
+});
